316f588a4667aff18d9c1e7e194bf6aa8330024c
Hamisakim <76621344+Hamisakim@users.noreply.github.com>
2024-12-14 15:44:25 +0000

    video has audio

--- src/app/jams/[id]/page.js
+++ src/app/jams/[id]/page.js
@@ -2,26 +2,20 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import WebcamRecorder from './WebcamRecorder';
 
-
 const page = async ({ params }) => {
   const { id } = await params;
   const getJamData = async () => {
     //does data getting
   };
 
-
-  
   return (
     <div>
-
-<WebcamRecorder />
-
       <div className="instruments ">
         <div>
           <h1>Strings</h1>
           {/* <FontAwesomeIcon icon='guitar' /> */}
 
-
+          <WebcamRecorder />
         </div>
       </div>
 
--- src/utils/firebase/queries.js
+++ src/utils/firebase/queries.js
@@ -1,19 +1,26 @@
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from './firebaseConfig';
 
-
 export const getJams = async () => {
   try {
     const jamsCol = collection(db, 'jams');
 
-
     const snapshot = await getDocs(jamsCol);
-    return snapshot.docs.map(doc => ({
+    return snapshot.docs.map((doc) => ({
       id: doc.id,
-      ...doc.data()
+      ...doc.data(),
     }));
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
   }
+};
+
+const getTrack = async (jamId, trackId) => {
+  const trackCol = collection(db, 'tracks');
+  const snapshot = await getDocs(jamsCol);
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
 };
